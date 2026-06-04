5624214e7947e6ab8716a5f1e8bc5d39f34b5744
Vindhya2006 <vindhyachoudary.dev@gmail.com>
2026-06-04 21:30:36 +0530

    fix orientation

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
         theme_color: '#1a8fe3',
         background_color: '#f0f7ff',
         display: 'standalone',
-        orientation: 'portrait',
+        orientation: 'any',
         scope: '/',
         start_url: '/',
         lang: 'en',
